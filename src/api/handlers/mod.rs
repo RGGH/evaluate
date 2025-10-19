b14fd93cac72ec8309cdd75d47158dd6b731a731
RGGH <pisscompany@gmail.com>
2025-10-19 14:58:25 +0100

    add-a-pre-judge-validation-layer-into-sql

--- src/api/handlers/mod.rs
+++ src/api/handlers/mod.rs
@@ -4,8 +4,11 @@ mod evals;
 mod experiments;
 mod history;
 pub mod ws;
+mod judge_prompts;
 
 pub use health::health_check;
 pub use evals::{run_eval, run_batch, get_eval, get_status, get_history, get_models};
 pub use experiments::{create_experiment, get_experiment};
 pub use ws::{ws_handler, WsBroker};
+
+pub use judge_prompts::*;
